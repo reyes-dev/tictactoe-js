76e201f38d90876b9d0ba4a4b72bd671342a0ddd
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-12-20 16:54:49 -0800

    Create Player object factory function

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+// This module is for storing the gameboard 
 const GameBoard = (() => {
-    const gameboard = [null, null, null,
-                        null, null, null,
-                        null, null, null];
-    return { gameboard };
+    const gameBoard = ["X", "O", "X",
+                        "O", "X", "O",
+                        "X", "O", "X"];
+    return { gameBoard };
 })();
+// This factory function for making players "O" and "X"
+const Player = (mark) => {
+    let mark = mark;
+};
