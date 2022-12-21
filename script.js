9a5fa9665254f8d0fde6019df7e4737341b62717
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-12-20 22:42:07 -0800

    Create display board function that renders gameboard array to page

--- script.js
+++ script.js
@@ -7,5 +7,19 @@ const GameBoard = (() => {
 })();
 // This factory function for making players "O" and "X"
 const Player = (mark) => {
-    let mark = mark;
+    return { mark };
 };
+// Creates buttons, sets their text to an array index and inserts them into DOM
+const displayController = (() => {
+    const main = document.querySelector('main');
+    const displayBoard = () => { 
+        for(let i = 0; i < GameBoard.gameBoard.length; i++) {
+            let button = document.createElement('button');
+            button.innerHTML = GameBoard.gameBoard[i];
+            main.appendChild(button);
+    }
+};
+    return { displayBoard };
+})();
+
+displayController.displayBoard();
